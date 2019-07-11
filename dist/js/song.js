74697761c5d1fd759a6388bcee10dea04c13f10f
weimo001 <48851999+weimo001@users.noreply.github.com>
2019-07-11 10:30:32 +0800

    xqr-7-10-10.30
    个人中心添加

--- dist/js/song.js
+++ dist/js/song.js
@@ -2,45 +2,25 @@
 $('#articleModal').off('shown.bs.modal').on('shown.bs.modal', function (e) {
     $(document).off('focusin.modal');
 });
-//KindEditor变量
-var editor;
+
 $(function () {
     //隐藏错误提示框
     $('.alert-danger').css("display", "none");
 
     initFlatPickr();
 
-    //详情编辑器
-    editor = KindEditor.create('textarea[id="editor"]', {
-        items: ['source', '|', 'undo', 'redo', '|', 'preview', 'print', 'template', 'code', 'cut', 'copy', 'paste',
-            'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
-            'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
-            'superscript', 'clearhtml', 'quickformat', 'selectall', '|', 'fullscreen', '/',
-            'formatblock', 'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold',
-            'italic', 'underline', 'strikethrough', 'lineheight', 'removeformat', '|', 'multiimage',
-            'table', 'hr', 'emoticons', 'baidumap', 'pagebreak',
-            'anchor', 'link', 'unlink'],
-        uploadJson: 'upload/file',
-        filePostName: 'file'
-    });
-
-    $('#articleModal').on('hidden.bs.modal', function () {
-        editor.html('请输入...');
-    })
-
     $('#articleModal').modal('hide');
 
     $("#jqGrid").jqGrid({
-        url: 'articles/list',
+        url: 'http://localhost:8888/song/SongList',
         datatype: "json",
         colModel: [
             {label: 'id', name: 'id', index: 'id', width: 50, key: true, hidden: true},
-            {label: '歌曲名', name: 'song', index: 'articleTitle', width: 80},
-            {label: '歌曲url', name: 'songurl', index: 'songurl', width: 80},
-            {label: '时长', name: 'duration ', index: 'duration', width: 80},
+            {label: '歌曲名', name: 'name', index: 'name', width: 80},
+            {label: '时长', name: 'musicTime', index: 'musicTime', width: 100},
             {label: '热度', name: 'hot', index: 'hot', width: 80},
-            {label: '图片', name: 'path', index: 'path', sortable: false, width: 100, formatter: imgFormatter},
-            {label: '发布时间', name: 'time', index: 'time', width: 80},
+            {label: '图片', name: 'musicHeadUrl', index: 'musicHeadUrl', sortable: false, width: 100, formatter: imgFormatter},
+            {label: '发布时间', name: 'songLongTime', index: 'songLongTime', width: 80},
 
         ],
         height: 560,
@@ -54,10 +34,11 @@ $(function () {
         multiselect: true,
         pager: "#jqGridPager",
         jsonReader: {
-            root: "data.list",
-            page: "data.currPage",
-            total: "data.totalPage",
-            records: "data.totalCount"
+            root: "items",
+            page: "currPage",
+            total: "totalPage",
+            records: "total"
+
         },
         prmNames: {
             page: "page",
@@ -100,6 +81,7 @@ $(function () {
     });
 
 });
+
 function imgFormatter(cellvalue) {
     return "<a href='" + cellvalue + "'> <img src='" + cellvalue + "' height=\"120\" width=\"135\" alt='path'/></a>";
 }
@@ -134,38 +116,32 @@ $('#saveButton').click(function () {
     if (validObject()) {
         //一切正常后发送网络请求
         //ajax
-        var id = $("#articleId").val();
-        var title = $("#articleName").val();
-        var addName = $("#articleAuthor").val();
-        var content = editor.html();
-        var data = {"articleTitle": title, "articleContent": content, "addName": addName};
-        var url = 'articles/save';
+        var id = $("#songId").val();
+        console.log("id:"+id);
+        var name = $("#name").val();
+        var time =  $('#musicTime').val();
+        var data = {"name": name, "time": time};
+        var url = 'http://localhost:8888/song/addSong';
         //id>0表示编辑操作
         if (id > 0) {
-            data = {"id": id, "articleTitle": title, "articleContent": content, "addName": addName};
-            url = 'articles/update';
+            var data = {"id": id, "name": name, "musicTime": time};
+            url = 'http://localhost:8888/song/updateSong';
         }
         $.ajax({
             type: 'POST',//方法类型
-            dataType: "json",//预期服务器返回的数据类型
+            //dataType: "json",//预期服务器返回的数据类型
             url: url,//url
             contentType: "application/json; charset=utf-8",
-            beforeSend: function (request) {
-                //设置header值
-                request.setRequestHeader("token", getCookie("token"));
-            },
             data: JSON.stringify(data),
             success: function (result) {
-                checkResultCode(result.resultCode);
-                if (result.resultCode == 200) {
-                    $('#articleModal').modal('hide');
+                if (result == "成功") {
+                    $('#songModal').modal('hide');
                     swal("保存成功", {
                         icon: "success",
                     });
                     reload();
-                }
-                else {
-                    $('#articleModal').modal('hide');
+                }else{
+                    $('#songModal').modal('hide');
                     swal("保存失败", {
                         icon: "error",
                     });
@@ -189,6 +165,9 @@ function articleAdd() {
     $('#articleModal').modal('show');
 }
 
+/**
+ * whg
+ */
 function articleEdit() {
     reset();
     $('.modal-title').html('编辑');
@@ -198,18 +177,16 @@ function articleEdit() {
         return;
     }
     //请求数据
-    $.get("articles/info/" + id, function (r) {
-        if (r.resultCode == 200 && r.data != null) {
+    $.get("http://localhost:8888/song/selectById?id=" + id, function (r) {
+        if (r != 400 ) {
             //填充数据至modal
-            $('#articleId').val(r.data.id);
-            $('#articleName').val(r.data.articleTitle);
-            $('#articleAuthor').val(r.data.addName);
-            editor.html('');
-            editor.html(r.data.articleContent);
+            $('#songId').val(r.id);
+            $('#name').val(r.name);
+            $('#musicTime').val(r.musicTime);
         }
     });
     //显示modal
-    $('#articleModal').modal('show');
+    $('#songModal').modal('show');
 }
 
 /**
@@ -218,6 +195,7 @@ function articleEdit() {
 function search() {
     //标题关键字
     var keyword = $('#keyword').val();
+    alert("keyword"+keyword);
     if (!validLength(keyword, 20)) {
         swal("搜索字段长度过大!", {
             icon: "error",
@@ -225,29 +203,12 @@ function search() {
         return false;
     }
 
-    /*//开始时间、结束时间
-    var startTimeStr = $('.startTime').val();
-    var endTimeStr = $('.endTime').val();
-    var startTime = new Date(startTimeStr.replace(/-/, "/"));
-    var endTime = new Date(endTimeStr.replace(/-/, "/"));
-
-    if (startTime >= endTime) {
-        swal("开始时间不能大于结束时间!", {
-            icon: "error"
-        });
-        return false;
-    }*/
-
-    //数据封装
-    var searchData = {keyword: keyword, startTime: startTimeStr, endTime: endTimeStr};
-
     //传入查询条件参数$("#jqGrid").jqGrid("setGridParam", {postData: searchData});
     $("#jqGrid").jqGrid("setGridParam");
     //点击搜索按钮默认都从第一页开始
     $("#jqGrid").jqGrid("setGridParam", {page: 1});
     //提交post并刷新表格
-    $("#jqGrid").jqGrid("setGridParam", {url: 'articles/search'}).trigger("reloadGrid");
-
+    $("#jqGrid").jqGrid("setGridParam", {url: 'http://localhost:8888/song/selectByName?name='+keyword}).trigger("reloadGrid");
 
 }
 
@@ -255,33 +216,25 @@ function search() {
  * 数据验证
  */
 function validObject() {
-    var articleName = $('#articleName').val();
-    if (isNull(articleName)) {
+    var name = $('#name').val();
+    if (isNull(name)) {
         showErrorInfo("标题不能为空!");
         return false;
     }
-    if (!validLength(articleName, 120)) {
+    if (!validLength(name, 120)) {
         showErrorInfo("标题字符不能大于120!");
         return false;
     }
-    var articleAuthor = $('#articleAuthor').val();
-    if (isNull(articleAuthor)) {
-        showErrorInfo("作者不能为空!");
+    var musicTime = $('#musicTime').val();
+    if (isNull(musicTime)) {
+        showErrorInfo("时间不能为空!");
         return false;
     }
-    if (!validLength(articleAuthor, 10)) {
-        showErrorInfo("作者字符不能大于10!");
+    if (!validLength(musicTime, 10)) {
+        showErrorInfo("时间不能太长!");
         return false;
     }
-    var ariticleContent = editor.html();
-    if (isNull(ariticleContent) || ariticleContent == '请输入...') {
-        showErrorInfo("内容不能为空!");
-        return false;
-    }
-    if (!validLength(ariticleContent, 8000)) {
-        showErrorInfo("内容字符不能大于8000!");
-        return false;
-    }
+
     return true;
 }
 
@@ -299,23 +252,20 @@ function reset() {
     $('#ariticleContent').val('');
 }
 
-function deleteArticle() {
+/**
+ * whg写
+ */
+function deleteSong() {
     var ids = getSelectedRows();
     if (ids == null) {
         return;
     }
     $.ajax({
-        type: "POST",
-        url: "articles/delete",
+        type: "GET",
+        url: "http://localhost:8888/song/delete?id="+ids,
         contentType: "application/json",
-        beforeSend: function (request) {
-            //设置header值
-            request.setRequestHeader("token", getCookie("token"));
-        },
-        data: JSON.stringify(ids),
         success: function (r) {
-            checkResultCode(r.resultCode);
-            if (r.resultCode == 200) {
+            if (r == "删除成功") {
                 swal("删除成功", {
                     icon: "success",
                 });
